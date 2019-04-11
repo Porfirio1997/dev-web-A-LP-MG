45a2a00b0b3f38938a41a773ed3f7a03ed2991e4
LucasPorfirioDaSilva <lucasporfirio1997@gmail.com>
2019-04-11 19:33:14 -0300

    resolvendo o banco

--- Claviculário Digital/main/static/data.js
+++ Claviculário Digital/main/static/data.js
@@ -1,6 +1,9 @@
+import {EventEmitter} from 'events'
+
+const store = new EventEmitter()
 const sqlite3 = require('sqlite3');
 
-function conectar() {
+store.conectar = function() {
   let db = new sqlite3.Database('./claviculario.db', (err) => {
     if (err) {
       return console.error(err.message);
@@ -8,13 +11,13 @@ function conectar() {
   });
 }
 
-function busca(sql) {
+store.select = function(sql) {
   db.all(sql, function (err, rows) {
     return (rows)
     });
   }
 
-function executar(sql) {
+store.CUD = function(sql) {
     db.run(sql, function (err) {
           if (err) {
             return console.log(err.message);
@@ -23,10 +26,12 @@ function executar(sql) {
       });
 }
 
-function desconectar(){
+store.desconectar = function(){
 db.close((err) => {
   if (err) {
     return console.error(err.message);
   }
   console.log('Close the database connection.');
 });}
+
+export default store
